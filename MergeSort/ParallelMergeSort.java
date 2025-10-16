9ef10e2a4ff9f8e20c9cc7a06b69504541c4dfbd
Bianca Oria <biancaoria@gmail.com>
2025-10-15 22:19:23 -0300

    Teste Merge Paralelo Fork

--- MergeSort/ParallelMergeSort.java
+++ MergeSort/ParallelMergeSort.java
@@ -1,64 +1,72 @@
 package MergeSort;
 
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
 public class ParallelMergeSort {
 
-    public void sort(int[] arr, int numeroThreads) {
-        parallelSort(arr, 0, arr.length - 1, numeroThreads);
+    public void sort(int[] arr, int numThreads) {
+        ForkJoinPool pool = new ForkJoinPool(numThreads);
+        pool.invoke(new MergeSortTask(arr, 0, arr.length - 1));
+        pool.shutdown();
     }
 
-    private void parallelSort(int[] arr, int inicio, int fim, int threadsDisponiveis) {
-        if (inicio < fim) {
-            if (threadsDisponiveis <= 1) {
+    private static class MergeSortTask extends RecursiveAction {
+        private final int[] arr;
+        private final int inicio;
+        private final int fim;
+        private static final int LIMITE_SEQUENCIAL = 10_000; // tamanho mínimo para usar threads
+
+        MergeSortTask(int[] arr, int inicio, int fim) {
+            this.arr = arr;
+            this.inicio = inicio;
+            this.fim = fim;
+        }
+
+        @Override
+        protected void compute() {
+            if (fim - inicio < LIMITE_SEQUENCIAL) {
                 sortSerial(arr, inicio, fim);
                 return;
             }
 
             int meio = (inicio + fim) / 2;
-            int threadsParaEsquerda = threadsDisponiveis / 2;
-            int threadsParaDireita = threadsDisponiveis - threadsParaEsquerda;
 
-            Thread leftThread = new Thread(() -> parallelSort(arr, inicio, meio, threadsParaEsquerda));
-            Thread rightThread = new Thread(() -> parallelSort(arr, meio + 1, fim, threadsParaDireita));
+            MergeSortTask leftTask = new MergeSortTask(arr, inicio, meio);
+            MergeSortTask rightTask = new MergeSortTask(arr, meio + 1, fim);
 
-            leftThread.start();
-            rightThread.start();
+            invokeAll(leftTask, rightTask);
+            merge(arr, inicio, meio, fim);
+        }
 
-            try {
-                leftThread.join();
-                rightThread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        private void sortSerial(int[] arr, int inicio, int fim) {
+            if (inicio < fim) {
+                int meio = (inicio + fim) / 2;
+                sortSerial(arr, inicio, meio);
+                sortSerial(arr, meio + 1, fim);
+                merge(arr, inicio, meio, fim);
             }
+        }
 
-            merge(arr, inicio, meio, fim);
+        private void merge(int[] arr, int inicio, int meio, int fim) {
+            int n1 = meio - inicio + 1;
+            int n2 = fim - meio;
+
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+
+            for (int i = 0; i < n1; i++) L[i] = arr[inicio + i];
+            for (int j = 0; j < n2; j++) R[j] = arr[meio + 1 + j];
+
+            int i = 0, j = 0, k = inicio;
+            while (i < n1 && j < n2) {
+                if (L[i] <= R[j]) arr[k++] = L[i++];
+                else arr[k++] = R[j++];
+            }
+            while (i < n1) arr[k++] = L[i++];
+            while (j < n2) arr[k++] = R[j++];
         }
     }
+// teste bostinha
 
-    private void sortSerial(int[] arr, int inicio, int fim) {
-        if (inicio < fim) {
-            int meio = (inicio + fim) / 2;
-            sortSerial(arr, inicio, meio);
-            sortSerial(arr, meio + 1, fim);
-            merge(arr, inicio, meio, fim);
-        }
-    }
-
-    private void merge(int[] arr, int inicio, int meio, int fim) {
-        int n1 = meio - inicio + 1;
-        int n2 = fim - meio;
-
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-
-        for (int i = 0; i < n1; i++) L[i] = arr[inicio + i];
-        for (int j = 0; j < n2; j++) R[j] = arr[meio + 1 + j];
-
-        int i = 0, j = 0, k = inicio;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) arr[k++] = L[i++];
-            else arr[k++] = R[j++];
-        }
-        while (i < n1) arr[k++] = L[i++];
-        while (j < n2) arr[k++] = R[j++];
-    }
 }
